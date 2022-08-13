ee1cb0cddb5d96e805060a9931496a547dd24200
Dmitrii Sobin <neo7976@yandex.ru>
2022-08-13 17:45:58 +0300

    Создание MedicalServiceImplTest.java initial настройка теста

--- src/test/java/ru/netology/patient/service/medical/MedicalServiceImplTest.java
+++ src/test/java/ru/netology/patient/service/medical/MedicalServiceImplTest.java
@@ -3,21 +3,35 @@ package ru.netology.patient.service.medical;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import ru.netology.patient.repository.PatientInfoFileRepository;
+import ru.netology.patient.repository.PatientInfoRepository;
+import ru.netology.patient.service.alert.SendAlertService;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MedicalServiceImplTest {
+    MedicalServiceImpl medicalService;
 
     @BeforeEach
     void setUp() {
+        System.out.println("Начало теста: ");
     }
 
     @AfterEach
     void tearDown() {
+        System.out.println("\nОкончание теста: ");
+        medicalService = null;
     }
 
     @Test
     void checkBloodPressure() {
+        PatientInfoRepository patientInfoRepository = Mockito.mock(PatientInfoRepository.class);
+
+        SendAlertService alertService = Mockito.mock(SendAlertService.class);
+
+
+        medicalService = new MedicalServiceImpl(patientInfoRepository, alertService);
     }
 
     @Test
