c001bbb6d09d5a811345f103e496eed7949659d8
Dmitrii Sobin <neo7976@yandex.ru>
2022-08-13 17:41:23 +0300

    Создание MedicalServiceImplTest.java

---
+++ src/test/java/ru/netology/patient/service/medical/MedicalServiceImplTest.java
@@ -0,0 +1,26 @@
+package ru.netology.patient.service.medical;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MedicalServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void checkBloodPressure() {
+    }
+
+    @Test
+    void checkTemperature() {
+    }
+}
